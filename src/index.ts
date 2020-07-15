986a93c94e4c8c63c72df00e80fc638d03e03a59
hpc <philipchanhk626@gmail.com>
2020-07-15 11:24:46 -0400

    proof of concept that metadata decorators is working

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
 import { router } from './routes/loginRoutes'
 import cookieSession from 'cookie-session'
+
+import { router as controllerRouter } from './controllers/decorators/controller'
+import './controllers/LoginController'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['someKey'] }))
 app.use(router)
+app.use(controllerRouter)
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
